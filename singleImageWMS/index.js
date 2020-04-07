d835f67c8d62701b07695de2e44addafd5f0375f
João Caetano <jcaetanodev@gmail.com>
2020-04-07 22:37:39 +0100

    Adicionei mais 3 exemplos (WMS)

---
+++ singleImageWMS/index.js
@@ -0,0 +1,30 @@
+import 'ol/ol.css';
+import Map from 'ol/Map';
+import View from 'ol/View';
+import {Image as ImageLayer, Tile as TileLayer} from 'ol/layer';
+import ImageWMS from 'ol/source/ImageWMS';
+import OSM from 'ol/source/OSM';
+
+
+var layers = [
+  new TileLayer({
+    source: new OSM()
+  }),
+  new ImageLayer({
+    extent: [-13884991, 2870341, -7455066, 6338219],
+    source: new ImageWMS({
+      url: 'https://ahocevar.com/geoserver/wms',
+      params: {'LAYERS': 'topp:states'},
+      ratio: 1,
+      serverType: 'geoserver'
+    })
+  })
+];
+var map = new Map({
+  layers: layers,
+  target: 'map',
+  view: new View({
+    center: [-10997148, 4569099],
+    zoom: 4
+  })
+});
